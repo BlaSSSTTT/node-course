0e999f278866917a76f730c52847bbc8c30a8f04
Vladyslav Antonchyk <109813312+BlaSSSTTT@users.noreply.github.com>
2024-06-25 19:48:03 +0300

    Using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,42 @@
 const path = require("path");
-
 const express = require("express");
+const hbs = require('hbs');
+
 const app = express();
 
-//  console.log(__dirname);
-//  console.log(path.join(__dirname,"../public"));
-
+//Define paths for Express config
 const publicDir = path.join(__dirname,"../public");
+const viewsPath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
 
+
+//Setup handlevars engine and views location
+app.set('view engine', 'hbs');
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDir));
 
+
+app.get("/",(req,res)=>{
+    res.render("index",{
+        title:"Weather app",
+        name: 'Vlad'
+    })
+})
+app.get("/about",(req,res)=>{
+    res.render("about",{
+        title:"About me",
+        name: 'Vlad'
+    })
+})
+app.get("/help",(req,res)=>{
+    res.render("help",{
+        title:"Help me",
+        name: 'Vlad'
+    })
+})
 app.get("/weather",(req,res)=>{
     res.send({
         forecast:123,
@@ -17,6 +44,21 @@ app.get("/weather",(req,res)=>{
     });
 })
 
+app.get('/help/*',(req,res)=>{
+    res.render("404",{
+        title: '404',
+        text: "help article not found",
+        name: 'Vlad'
+    })
+})
+
+app.get("*",(req,res)=>{
+    res.render("404",{
+        title: '404',
+        text: "help article not found",
+        name: 'Vlad'
+    })
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000.");
 })
