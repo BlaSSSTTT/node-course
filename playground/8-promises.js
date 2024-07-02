a639d5adbea891e287b3835408c419f475fe1762
Vladyslav Antonchyk <109813312+BlaSSSTTT@users.noreply.github.com>
2024-07-02 18:52:39 +0300

    Done REST API and seperate routes files

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,30 @@
-const doWorkPromise = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        //resolve([1,5,7]);
-        reject("Error");
-    },2000);
-});
+const add = (a,b)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(a+b);
+        },2000);
+    })
+}
 
-doWorkPromise.then((data)=>{
-    console.log(data);
+// add(2,1).then((data)=>{
+//     console.log(data);
+//     add(data,5).then((data1)=>{
+//         console.log(data1);
+//     }).catch((error1)=>{
+//         console.log(error1);
+//     })
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+add(1,1).then((sum)=>{
+    console.log(sum);
+    return add(sum,1);
+}).then((sum)=>{
+    console.log(sum);
 }).catch((error)=>{
     console.log(error);
 })
+
+
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,74 +1,14 @@
 const express = require("express");
 require('./db/mongoose.js');
-const User = require("./models/user.js");
-const Task = require("./models/task.js");
+const userRouter = require("./routers/user.js")
+const taskRouter = require("./routers/task.js")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users",(req,res)=>{
-    const user = new User(req.body)
-
-    user.save().then((body)=>{
-        res.status(201).send(body);
-    }).catch((error)=>{
-        res.status(400).send(error);
-    })
-})
-
-app.get("/users",(req,res)=>{
-    User.find({}).then((data)=>{
-        res.send(data);
-    }).catch((error)=>{
-        res.status(500).send();
-    })
-})
-
-app.get("/users/:id",(req,res)=>{
-    const _id = req.params.id;
-    User.findById(_id).then((user)=>{
-        if(!user){
-            return res.status(404).send();
-        }
-        res.send(user);
-    }).catch((error)=>{
-        res.status(500).send();
-    })
-})
-
-
-
-app.post("/tasks",(req,res)=>{
-    const task = new Task(req.body);
-
-    task.save().then((data)=>{
-        res.status(201).send(data)
-    }).catch((error)=>{
-        res.status(400).send(error);
-    })
-})
-
-app.get("/tasks",(req,res)=>{
-    Task.find({}).then((data)=>{
-        res.send(data);
-    }).catch((error)=>{
-        res.status(500).send();
-    })
-})
-
-app.get("/tasks/:id",(req,res)=>{
-    const _id = req.params.id;
-    Task.findById(_id).then((task)=>{
-        if(!task){
-            return res.status(404).send()
-        }
-        res.send(task);
-    }).catch((error)=>{
-        res.status(500).send();
-    })
-})
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, ()=>{
     console.log("Server is running on port "+ port);
